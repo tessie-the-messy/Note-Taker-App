7bfe93a8b862cfb63645c3f1a3a928ff261c4404
Tessa Elyse <tessa.elise.robinson@gmail.com>
2022-11-30 20:07:14 -0700

    fixed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path")
 const notes = require("./db/db.json")
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
